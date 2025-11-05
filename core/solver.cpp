304e98348fb414230d592e7db82118347a6a9935
JdeJong <JdeJong1990@users.noreply.github.com>
2025-11-05 16:42:02 +0100

    Add comment for clarity

--- core/solver.cpp
+++ core/solver.cpp
@@ -3,11 +3,13 @@
 
 void Solver::step(Grid& grid, float dt) {
     // Temporarily just write the old values to the new grid directly.
-    for (int x = 0; x<grid.getWidth(); x++){
-        for (int y = 0; y<grid.getHeight(); y++){
-            grid.smoke_next[grid.idx(x,y)] = grid.smoke[grid.idx(x,y)];
-        }
-    }
+    // for (int x = 0; x<grid.getWidth(); x++){
+    //     for (int y = 0; y<grid.getHeight(); y++){
+    //         grid.smoke_next[grid.idx(x,y)] = grid.smoke[grid.idx(x,y)];
+    //     }
+    // }
+    flowTransform(grid, dt);
+
     swapPointers(grid);
 }
 
@@ -18,4 +20,21 @@ void Solver::swapPointers(Grid& grid){
     std::swap(grid.smoke, grid.smoke_next);
     std::swap(grid.temperature, grid.temperature_next);
     std::swap(grid.mass, grid.mass_next);
+}
+
+//semi-Lagrangian advection
+void applyAdvection(Grid& grid, float dt){
+    weights = computeWeights(grid, dt);
+    interpolateField(Grid, weights);
+}
+
+InterpolationWeights computeWeights(Grid& grid, float dt){
+    
+    InterpolationWeights weights;
+
+    return weights;
+}
+
+void interpolateField(Grid& grid, InterpolationWeights weigths){
+    //Interpolate between cells that influence the cell of interest.
 }
