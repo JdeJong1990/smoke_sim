e1be1d76ddfe4e51c181b5064e14d69eee739f95
JdeJong <JdeJong1990@users.noreply.github.com>
2025-11-18 16:19:58 +0100

    Added missing fields to render modes.

--- core/renderer.cpp
+++ core/renderer.cpp
@@ -25,15 +25,19 @@ void Renderer::pollEvents() {
 
 void Renderer::draw(const Grid& grid, InputHandler& input) {
     window.clear(sf::Color::Black);
-    if (input.isDown(sf::Keyboard::Key::V)){
+    if (input.isDown(sf::Keyboard::Key::U)){
+        render_mode = 'U';
+    } else if (input.isDown(sf::Keyboard::Key::V)){
         render_mode = 'V';
-        std::cout << "Render mode is now V\n";
-    }
-    if (input.isDown(sf::Keyboard::Key::S)){
+    } else if (input.isDown(sf::Keyboard::Key::P)){
+        render_mode = 'P';
+    } else if (input.isDown(sf::Keyboard::Key::S)){
         render_mode = 'S';
-        std::cout << "Render mode is now S\n";
+    } else if (input.isDown(sf::Keyboard::Key::T)){
+        render_mode = 'T';
+    } else if (input.isDown(sf::Keyboard::Key::M)){
+        render_mode = 'M';
     }
-
     
     for (int y = 0; y < grid.getHeight(); ++y) {
         for (int x = 0; x < grid.getWidth(); ++x) {
@@ -48,6 +52,18 @@ void Renderer::draw(const Grid& grid, InputHandler& input) {
             switch(render_mode){
                 case 'V':
                 value = grid.v[grid.idx(x, grid.getHeight() -1 - y)];
+                break;
+                case 'U':
+                value = grid.u[grid.idx(x, grid.getHeight() -1 - y)];
+                break;
+                case 'P':
+                value = grid.pressure[grid.idx(x, grid.getHeight() -1 - y)];
+                break;
+                case 'T':
+                value = grid.temperature[grid.idx(x, grid.getHeight() -1 - y)];
+                break;
+                case 'M':
+                value = grid.mass[grid.idx(x, grid.getHeight() -1 - y)];
                 break;
                 default: 
                 value = grid.smoke[grid.idx(x, grid.getHeight() -1 - y)];
